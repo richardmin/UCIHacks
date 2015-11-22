f49778ff3f0d245bbfe45fee19d5e7798d3fdfba
Richard Min <richardmin97@gmail.com>
2015-11-21 16:34:59 -0800

    no message

---
+++ Iteration1/Assets/Scripts/Balloon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ballon : MonoBehaviour {
+
+	public GameObject knife; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider collision)
+	{
+		GameObject objectHit = collision.gameObject;
+		
+		if (objectHit == knife)
+		{
+			//Update Player Inventory with Bubble
+
+			Destroy(this.gameObject);
+		}
+		
+	}
+}
